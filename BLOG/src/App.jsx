773e8ba72de8f46d9af5272cd5b7ed538b9a4ca9
nandani0408 <nandanimpatel@gmail.com>
2024-12-12 17:48:23 +0530

    Add routes and pages

--- BLOG/src/App.jsx
+++ BLOG/src/App.jsx
@@ -1,8 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
-    <div className="text-3xl text-red-500">
-      app
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
